6c6d59f2cd80dc4f4882e8b9221a9246a964e6f2
usmanGhani16 <afro123737@gmail.com>
2020-11-16 23:48:05 +0500

    start learning functional programmin

---
+++ code/fn.js
@@ -0,0 +1,62 @@
+// Functional Programming: Learn About Functional Programming
+// Function that returns a string representing a cup of green tea
+const prepareTea = () => 'greenTea';
+
+/*
+Given a function (representing the tea type) and number of cups needed, the
+following function returns an array of strings (each representing a cup of
+a specific type of tea).
+*/
+const getTea = (numOfCups) => {
+  const teaCups = [];
+
+  for (let cups = 1; cups <= numOfCups; cups += 1) {
+    const teaCup = prepareTea();
+    teaCups.push(teaCup);
+  }
+  return teaCups;
+};
+
+const tea4TeamFCC = getTea(40);
+
+// Functional Programming: Understand Functional Programming Terminology
+
+// Function that returns a string representing a cup of green tea
+const prepareGreenTea = () => 'greenTea';
+
+// Function that returns a string representing a cup of black tea
+const prepareBlackTea = () => 'blackTea';
+
+/*
+Given a function (representing the tea type) and number of cups needed, the
+following function returns an array of strings (each representing a cup of
+a specific type of tea).
+*/
+const getTea = (prepareTea, numOfCups) => {
+  const teaCups = [];
+
+  for (let cups = 1; cups <= numOfCups; cups += 1) {
+    const teaCup = prepareTea();
+    teaCups.push(teaCup);
+  }
+  return teaCups;
+};
+
+// Only change code below this line
+const tea4GreenTeamFCC = getTea(prepareGreenTea, 27);
+const tea4BlackTeamFCC = getTea(prepareBlackTea, 13);
+// Only change code above this line
+
+console.log(tea4GreenTeamFCC, tea4BlackTeamFCC);
+
+// Functional Programming: Understand the Hazards of Using Imperative Code
+// Functional Programming: Avoid Mutations and Side Effects Using Functional Programming
+
+// The global variable
+var fixedValue = 4;
+
+function incrementer(arg) {
+  return arg + 1;
+}
+
+// Functional Programming: Pass Arguments to Avoid External Dependence in a Function
