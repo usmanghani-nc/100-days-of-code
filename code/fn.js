3a0f4614d34819f269630fc9bb12fe3fc388ba1d
usmanGhani16 <afro123737@gmail.com>
2020-11-18 00:57:19 +0500

    myMap

--- code/fn.js
+++ code/fn.js
@@ -1,63 +1,63 @@
 // Functional Programming: Learn About Functional Programming
 // Function that returns a string representing a cup of green tea
-const prepareTea = () => 'greenTea';
+// const prepareTea = () => 'greenTea';
 
-/*
-Given a function (representing the tea type) and number of cups needed, the
-following function returns an array of strings (each representing a cup of
-a specific type of tea).
-*/
-const getTea = (numOfCups) => {
-  const teaCups = [];
+// /*
+// Given a function (representing the tea type) and number of cups needed, the
+// following function returns an array of strings (each representing a cup of
+// a specific type of tea).
+// */
+// const getTea = (numOfCups) => {
+//   const teaCups = [];
 
-  for (let cups = 1; cups <= numOfCups; cups += 1) {
-    const teaCup = prepareTea();
-    teaCups.push(teaCup);
-  }
-  return teaCups;
-};
+//   for (let cups = 1; cups <= numOfCups; cups += 1) {
+//     const teaCup = prepareTea();
+//     teaCups.push(teaCup);
+//   }
+//   return teaCups;
+// };
 
-const tea4TeamFCC = getTea(40);
+// const tea4TeamFCC = getTea(40);
 
-// Functional Programming: Understand Functional Programming Terminology
+// // Functional Programming: Understand Functional Programming Terminology
 
-// Function that returns a string representing a cup of green tea
-const prepareGreenTea = () => 'greenTea';
+// // Function that returns a string representing a cup of green tea
+// const prepareGreenTea = () => 'greenTea';
 
-// Function that returns a string representing a cup of black tea
-const prepareBlackTea = () => 'blackTea';
+// // Function that returns a string representing a cup of black tea
+// const prepareBlackTea = () => 'blackTea';
 
-/*
-Given a function (representing the tea type) and number of cups needed, the
-following function returns an array of strings (each representing a cup of
-a specific type of tea).
-*/
-const getTea = (prepareTea, numOfCups) => {
-  const teaCups = [];
+// /*
+// Given a function (representing the tea type) and number of cups needed, the
+// following function returns an array of strings (each representing a cup of
+// a specific type of tea).
+// */
+// const getTea = (prepareTea, numOfCups) => {
+//   const teaCups = [];
 
-  for (let cups = 1; cups <= numOfCups; cups += 1) {
-    const teaCup = prepareTea();
-    teaCups.push(teaCup);
-  }
-  return teaCups;
-};
+//   for (let cups = 1; cups <= numOfCups; cups += 1) {
+//     const teaCup = prepareTea();
+//     teaCups.push(teaCup);
+//   }
+//   return teaCups;
+// };
 
 // Only change code below this line
-const tea4GreenTeamFCC = getTea(prepareGreenTea, 27);
-const tea4BlackTeamFCC = getTea(prepareBlackTea, 13);
-// Only change code above this line
+// const tea4GreenTeamFCC = getTea(prepareGreenTea, 27);
+// const tea4BlackTeamFCC = getTea(prepareBlackTea, 13);
+// // Only change code above this line
 
-console.log(tea4GreenTeamFCC, tea4BlackTeamFCC);
+// console.log(tea4GreenTeamFCC, tea4BlackTeamFCC);
 
 // Functional Programming: Understand the Hazards of Using Imperative Code
 // Functional Programming: Avoid Mutations and Side Effects Using Functional Programming
 
 // The global variable
-var fixedValue = 4;
+// var fixedValue = 4;
 
-function incrementer(arg) {
-  return arg + 1;
-}
+// function incrementer(arg) {
+//   return arg + 1;
+// }
 
 // Functional Programming: Pass Arguments to Avoid External Dependence in a Function
 
@@ -72,3 +72,38 @@ function incrementer(arg) {
 //   };
 
 // Functional Programming: Implement map on a Prototype
+
+var s = [23, 65, 98, 5];
+
+Array.prototype.myMap = function (callback) {
+  var newArray = [];
+
+  var items = this;
+
+  for (const item of items) {
+    newArray.push(callback(item));
+  }
+
+  return newArray;
+};
+
+var new_s = s.myMap(function (item) {
+  return item * 2;
+});
+
+console.log(new_s);
+
+console.log(Array.prototype, 'dad ?');
+
+// Functional Programming: Use the filter Method to Extract Data from an Array
+
+var newArr = watchList.map((el, idx) => {
+  return {
+    title: el.Title,
+    rating: el.imdbRating,
+  };
+});
+
+var filteredList = newArr.filter((el, idx) => el.rating >= 8.0);
+
+console.log(filteredList);
