f5cc081b67d90068c2284026b682ce92f6480a00
usmanGhani16 <afro123737@gmail.com>
2020-11-18 23:18:34 +0500

    making filter

--- code/fn.js
+++ code/fn.js
@@ -107,3 +107,26 @@ var newArr = watchList.map((el, idx) => {
 var filteredList = newArr.filter((el, idx) => el.rating >= 8.0);
 
 console.log(filteredList);
+
+// The global variable
+var s = [23, 65, 98, 5];
+
+Array.prototype.myFilter = function (callback) {
+  // Only change code below this line
+  var newArray = [];
+
+  var items = this;
+
+  for (const item of items) {
+    newArray.push(callback(item));
+  }
+
+  // Only change code above this line
+  return newArray;
+};
+
+var new_s = s.myFilter(function (item) {
+  return item % 2 === 1;
+});
+
+console.log(new_s, 'filter');
