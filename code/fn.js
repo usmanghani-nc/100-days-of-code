1c5aab28ce2505d5b9b13169fa67f56abee2b6bb
usmanGhani16 <afro123737@gmail.com>
2020-11-21 23:07:22 +0500

    finished fn module

--- code/fn.js
+++ code/fn.js
@@ -367,4 +367,74 @@ function nonMutatingSort(arr) {
   return newArr;
   // Only change code above this line
 }
-console.log(nonMutatingSort(globalArray));
+// console.log(nonMutatingSort(globalArray));
+
+// Functional Programming: Split a String into an Array Using the split Method
+
+function splitify(str) {
+  // Only change code below this line
+
+  return str.split(/\W/);
+  // Only change code above this line
+}
+// console.log(splitify('Hello World,I-am code'));
+
+// Functional Programming: Combine an Array into a String Using the join Method
+
+function sentensify(str) {
+  // Only change code below this line
+
+  return str.split(/\W/).join(' ');
+  // Only change code above this line
+}
+
+console.log(sentensify('May-the-force-be-with-you'));
+
+// Functional Programming: Apply Functional Programming to Convert Strings to URL Slugs
+function urlSlug(title) {
+  const str = title.trim().toLowerCase().split(' ');
+
+  const arr = str.map((el) => {
+    const result = [];
+
+    if (!el) {
+      return;
+    }
+    result.push(el);
+
+    return result.join(' ');
+  });
+
+  return arr.filter((el) => el !== undefined).join('-');
+}
+// console.log(urlSlug(' Winter Is  Coming'));
+
+// Functional Programming: Use the every Method to Check that Every Element in an Array Meets a Criteria
+
+// function checkPositive(arr) {
+//   // Only change code below this line
+
+//   return arr.every((curr) => curr > -1);
+//   // Only change code above this line
+// }
+
+// console.log(checkPositive([1, 2, 3, -4, 5]));
+
+// Functional Programming: Use the some Method to Check that Any Elements in an Array Meet a Criteria
+function checkPositive(arr) {
+  // Only change code below this line
+
+  return arr.some((curr) => curr > -1);
+  // Only change code above this line
+}
+
+console.log(checkPositive([1, 2, 3, -4, 5]));
+
+// Functional Programming: Introduction to Currying and Partial Application
+function add(x) {
+  // Only change code below this line
+  return (y) => (z) => x + y + z;
+  // Only change code above this line
+}
+
+console.log(add(10)(20)(30));
