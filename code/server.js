6a8cad86f01136a3b58918a2ecd72c446a841a9c
usmanGhani16 <afro123737@gmail.com>
2020-12-16 21:56:48 +0500

    finished node expres basics

--- code/server.js
+++ code/server.js
@@ -1,14 +1,54 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 require('dotenv/config');
 const app = express();
 
 app.listen(3201);
 
+const midWare = (req, res, next) => {
+  console.log(req.method, req.path, '-', req.ip);
+
+  next();
+};
+
+const timeWare = (req, res, next) => {
+  req.time = new Date().toString();
+  next();
+};
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// app.use(midWare);
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/flex2.html');
+  res.sendFile(__dirname + '/form.html');
 });
 
 app.use('/', express.static(__dirname + '/'));
+
+// app.get('/now', timeWare, (req, res) => {
+//   res.json({ time: req.time });
+// });
+
+app.get('/:word/echo', (req, res) => {
+  const { word } = req.params;
+
+  res.json({ echo: word });
+});
+
+app.get('/name', (req, res) => {
+  const { first, last } = req.query;
+
+  res.json({ name: `${first} ${last}` });
+});
+
+app.post('/name', (req, res) => {
+  console.log(req.body);
+  const { first, last } = req.body;
+
+  res.send({ name: `${first} ${last}` });
+});
 
 app.get('/json', (req, res) => {
   res.json({
